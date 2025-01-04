9ffee35087a082b83e5d966804c0cdd165802170
Rakibul Hassan <107980584+DurjoyRakibul@users.noreply.github.com>
2025-01-04 14:30:41 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify, send_file
+from flask_cors import CORS  # Import CORS
 import qrcode
 from io import BytesIO
-from PIL import Image
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Route for the home page
 @app.route('/')
